8c5e51713765662bcfffc224e805c20db8fecffb
Daniyal-Shah <daniyalshah7862018@gmail.com>
2023-05-15 23:03:57 +0500

    components exported from locofy zip project

---
+++ GlobalStyles.ts
@@ -0,0 +1,12 @@
+/* fonts */
+export const FontFamily = {
+  novaRoundRegular: "Nova Round_regular",
+};
+/* Colors */
+export const Color = {
+  white: "#fff",
+};
+/* border radiuses */
+export const Border = {
+  br_21xl: 40,
+};
